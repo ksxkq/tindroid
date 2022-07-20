3c7bab996f6090b04e3dbedb974b8985c2c558e4
xukeqiang <312965601@qq.com>
2022-07-20 19:58:59 +0800

    function and theme toolbar

--- app/src/main/java/co/tinode/tindroid/BaseFragmentActivity.java
+++ app/src/main/java/co/tinode/tindroid/BaseFragmentActivity.java
@@ -1,20 +1,29 @@
 package co.tinode.tindroid;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public abstract class BaseFragmentActivity extends FragmentActivity {
+public abstract class BaseFragmentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutRes());
+        setStatusBarColor();
 
         FragmentManager fm = getSupportFragmentManager();
 
@@ -23,6 +32,34 @@ public abstract class BaseFragmentActivity extends FragmentActivity {
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
 
         tx.commitAllowingStateLoss();
+    }
+
+    protected void initToolbar(@StringRes int titleRes) {
+        initToolbar(getResources().getString(titleRes));
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setStatusBarColor() {
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.white));
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+
+    protected void initToolbar(String title) {
+        Toolbar mToolbarView = findViewById(R.id.toolbar);
+        mToolbarView.setTitle(title);
+        mToolbarView.setBackgroundColor(getResources().getColor(R.color.white));
+        setSupportActionBar(mToolbarView);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(true);
+            actionBar.setDisplayUseLogoEnabled(false);
+            actionBar.setHomeButtonEnabled(true);
+        }
     }
 
     abstract Fragment getFragment();
--- app/src/main/java/co/tinode/tindroid/FindByIDActivity.java
+++ app/src/main/java/co/tinode/tindroid/FindByIDActivity.java
@@ -1,8 +1,18 @@
 package co.tinode.tindroid;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FindByIDActivity extends BaseFragmentActivity{
+public class FindByIDActivity extends BaseFragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initToolbar(R.string.action_add_contact);
+    }
+
     @Override
     Fragment getFragment() {
         return new AddByIDFragment();
